2b559fd08ba516bbfaa3509d3e142dbf68761d99
kirubhavignesh <imkiru7@gmail.com>
2023-12-21 20:37:48 +0530

    Updates

---
+++ precache-manifest.44087eab80dfda10ccfaa79b0dd544e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33b62687529e59ba3edd20462e74cebf",
+    "url": "/secret-santa/index.html"
+  },
+  {
+    "revision": "17bb68c795a81a0e3278",
+    "url": "/secret-santa/static/css/main.6d7721f7.chunk.css"
+  },
+  {
+    "revision": "89dcb654bb7d515ceb9e",
+    "url": "/secret-santa/static/js/2.8aa3423f.chunk.js"
+  },
+  {
+    "revision": "17bb68c795a81a0e3278",
+    "url": "/secret-santa/static/js/main.122f6503.chunk.js"
+  },
+  {
+    "revision": "54ab70af36e565caa615",
+    "url": "/secret-santa/static/js/runtime~main.a79445ef.js"
+  }
+]);
